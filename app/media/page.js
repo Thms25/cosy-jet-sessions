56c503ad20601ec9758c7716a16f914b3c0ad17c
thomas allen <thomasallenmartinho@gmail.com>
2023-07-20 09:53:13 +0200

    init 1

---
+++ app/media/page.js
@@ -0,0 +1,27 @@
+import React from "react";
+import YouTube from "react-youtube";
+
+export default class YoutubeVideo
+extends React.Component {
+render() {
+	const opts = {
+	height: "390",
+	width: "640",
+	playerVars: {
+		autoplay: 1,
+	},
+	};
+
+	return (
+	<div>
+		<h3>Luidji - Gisèle | Cover by Orlane</h3>
+		<YouTube videoId="jZrB_2hL0bk"
+			opts={opts} onReady={this._onReady} />
+	</div>
+	);
+}
+
+_onReady(event) {
+	event.target.pauseVideo();
+}
+}
--- app/page.js
+++ app/page.js
@@ -1,24 +1,32 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>
+            <Link href="/media">
+              Media
+            </Link>
+          </li>
+          <li>Contact</li>
+        </ul>
+      </nav>
       <header>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Media</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-        <Image
-          priority
-          src="/images/cjs-banner.png"
-          alt='cjs-banner'
-          width={1710}
-          height={751}
-        />
+        <div className='banner'>
+          <Image
+            id='cjs-banner'
+            priority
+            src="/images/cjs-banner.png"
+            alt='cjs-banner'
+            width={1710}
+            height={751}
+          />
+        </div>
       </header>
     </main>
   )
