64cf2da5723d2fa4b5bf1e0b7e5a1fdd6de99a26
thomas allen <thomasallenmartinho@gmail.com>
2023-07-22 17:44:10 +0200

    organized styles

--- app/media/page.js
+++ app/media/page.js
@@ -1,33 +1,6 @@
-// import React from "react";
 // import YouTube from "react-youtube";
-
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "components/media.module.scss"
-
-// export default class YoutubeVideo
-// extends React.Component {
-// render() {
-// 	const opts = {
-// 	height: "390",
-// 	width: "640",
-// 	playerVars: {
-// 		autoplay: 1,
-// 	},
-// 	};
-
-// 	return (
-// 	<div>
-// 		<h3>Luidji - Gisèle | Cover by Orlane</h3>
-// 		<YouTube videoId="jZrB_2hL0bk"
-// 			opts={opts} onReady={this._onReady} />
-// 	</div>
-// 	);
-// }
-
-// _onReady(event) {
-// 	event.target.pauseVideo();
-// }
-// }
 
 export default function Media() {
   return (
@@ -37,12 +10,14 @@ export default function Media() {
         <p>Welcme to the media page</p>
       </div>
       <div className={styles.images}>
-        <Image
+        {/* <Image
           src="/images/decor.png"
           alt='cjs-decor'
           width={1850}
           height={1044}
-        />
+        /> */}
+      </div>
+      <div className={styles.videos}>
       </div>
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import styles from 'components/home.module.scss';
+
 export default function Home() {
   return (
     <div>
@@ -15,16 +17,23 @@ export default function Home() {
         </div>
       </header>
       <div>
-        <section>
+        <section className={styles.section}>
           <h1>Browse through all our content</h1>
-          <div className='concept-section'>
-            <div className='concept-card'>
-              Videos
+          <div className={styles.concept}>
+            <div className={styles.card}>
+              Events
+              {/* <video src="https://www.youtube.com/watch?v=jZrB_2hL0bk" controls autoplay></video> */}
             </div>
-            <div className='concept-card'>
+            <div className={styles.card}>
               interviews
+              {/* <Image
+                src="/images/lights.png"
+                alt='cjs-banner'
+                width={732}
+                height={1206}
+              /> */}
             </div>
-            <div className='concept-card'>
+            <div className={styles.card}>
               podcasts
             </div>
           </div>
