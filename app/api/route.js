3b8f79212474f8a2c2c41bebef95cdfbc6e3f4c0
thomas allen <thomasallenmartinho@gmail.com>
2023-08-02 21:09:59 +0200

    comp created to connect db

---
+++ app/api/route.js
@@ -0,0 +1,15 @@
+import { sql } from "@vercel/postgres";
+
+export default async function VideoIds({ params }) {
+  // const { rows } = await sql`SELECT * from VIDEO_ID`;
+
+  // return (
+  //   <div>
+  //     {rows.map((row) => (
+  //       <div key={row.id}>
+  //         {row.id} - {row.video_yt_id}
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
+}
