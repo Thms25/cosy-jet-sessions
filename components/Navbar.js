0ad98aaf56cb12fccaccc68bf2c08b6b589b40f9
thomas allen <thomasallenmartinho@gmail.com>
2023-08-12 21:05:25 +0200

    added animation for fun

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from "../styles/navbar.module.scss";
 import Link from "next/link";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
+    // desktop
     <nav className={styles.nav}>
+      <Image
+        id={styles.cjsIcon}
+        src="/images/cjsIcon.png"
+        alt="cjs icon"
+        width={33}
+        height={34}
+      />
       <ul>
         <li>
           <Link href="/">Home</Link>
@@ -19,6 +30,7 @@ const Navbar = () => {
           <Link href="/contact">Contact</Link>
         </li>
       </ul>
+      <FontAwesomeIcon icon={faUser} className={styles.icon} />
     </nav>
   );
 };
