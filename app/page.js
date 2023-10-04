151df9e6df6878cefae6390cff656ba21be49a76
thomas allen <thomasallenmartinho@gmail.com>
2023-10-04 16:06:20 +0200

    home page done

--- app/page.js
+++ app/page.js
@@ -1,48 +1,11 @@
-import Image from "next/image";
 import styles from "../styles/home.module.scss";
-import videoStyles from "../styles/videos.module.scss";
-import YoutubeVideo from "../components/YoutubeVideo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import CjsBanner from "@/components/CjsBanner";
 import CarousselHome from "@/components/caroussels/CarousselHome";
-
-async function getRecentVideos() {
-  const res = await fetch("api/getRecentVideos");
-  consle.log(res);
-  const data = await res.json();
-  console.log(data);
-  return data;
-}
+import RecentVideos from "@/components/videos/RecentVideos";
 
 export default async function Home() {
-  // const [videos, setVideos] = useState([]);
-  // const videos = await getRecentVideos();
-  const videos = [];
-  // console.log(videos);
-
-  // const [carouLoaded, setCarouLoaded] = useState(false);
-  // const [recentVidsLoaded, setRecentVidsLoaded] = useState(false);
-
-  // const [firstVideLoaded, setFirstVidLoaded] = useState(false);
-  // const handleVideoLoaded = (loaded) => {
-  //   setFirstVidLoaded(loaded);
-  // };
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setCarouLoaded(true);
-  //   }, 500);
-  //   async function fetchVideos() {
-  //     // const videoData = await getRecentVideos();
-  //     // setVideos(videoData);
-  //     setRecentVidsLoaded(true);
-  //   }
-  //   if (carouLoaded) {
-  //     fetchVideos();
-  //   }
-  // }, [carouLoaded]);
-
   return (
     <div className={styles.home}>
       <header className={styles.header}>
@@ -67,15 +30,7 @@ export default async function Home() {
             <h4>Check out our most recent uploads</h4>
             <div className={styles.recentVideosWrap}>
               <FontAwesomeIcon icon={faAngleLeft} className={styles.icon} />
-              <div className={videoStyles.recentVideos}>
-                {videos?.map((video) => (
-                  <YoutubeVideo
-                    key={video.id}
-                    videoId={video.videoId}
-                    iframeClassName="homeVidFrame"
-                  />
-                ))}
-              </div>
+              <RecentVideos />
               <FontAwesomeIcon icon={faAngleRight} className={styles.icon} />
             </div>
           </div>
