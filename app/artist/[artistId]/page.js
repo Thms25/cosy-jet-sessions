81f4db645cc5b7e4f4b777cedd92a4663cacc8c4
thomas allen <thomasallenmartinho@gmail.com>
2023-10-07 13:48:43 +0200

    artist page performance fixed

--- app/artist/[artistId]/page.js
+++ app/artist/[artistId]/page.js
@@ -1,37 +1,20 @@
-"use client";
-
-import YoutubeVideo from "../../../components/YoutubeVideo";
-import { useEffect, useState } from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
 import styles from "/styles/artistPage.module.scss";
+import CarousselArtist from "@/components/caroussels/CarousselArtist";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShare } from "@fortawesome/free-solid-svg-icons";
 
 async function getArtist(id) {
-  const res = await fetch(`../api/getArtist?artistId=${id}`);
+  const res = await fetch(`${process.env.URL}/api/getArtist?artistId=${id}`);
   return res.json();
 }
 
-export default function Page({ params }) {
-  const [artist, setArtist] = useState([]);
-  const [videoLoaded, setVideoLoaded] = useState(false);
-
-  const handleVideoLoaded = (loaded) => {
-    setVideoLoaded(loaded);
-  };
-
-  useEffect(() => {
-    async function fetchArtist() {
-      const result = await getArtist(params.artistId);
-      setArtist(result);
-    }
-    fetchArtist();
-  }, [params.artistId]);
+export default async function Page({ params }) {
+  const artist = await getArtist(params.artistId);
+  console.log(artist);
 
   return (
     <div>
-      {artist.videos ? (
+      {artist.videos && (
         <div>
           <div>
             <h1>{artist.name}</h1>
@@ -43,31 +26,9 @@ export default function Page({ params }) {
                 <FontAwesomeIcon icon={faShare} rotation={90} />
               </p>
             </div>
-            <Carousel className={styles.caroussel} showStatus={false}>
-              {artist.videos.map((video, index) => (
-                <div key={video.id}>
-                  {index === 0 ? (
-                    <YoutubeVideo
-                      videoId={video.videoId}
-                      iframeClassName="video"
-                      onVideoLoaded={handleVideoLoaded}
-                    />
-                  ) : (
-                    videoLoaded && (
-                      <YoutubeVideo
-                        videoId={video.videoId}
-                        iframeClassName="video"
-                        onVideoLoaded={handleVideoLoaded}
-                      />
-                    )
-                  )}
-                </div>
-              ))}
-            </Carousel>
+            <CarousselArtist videos={artist.videos} />
           </div>
         </div>
-      ) : (
-        <p>Loading...</p>
       )}
     </div>
   );
