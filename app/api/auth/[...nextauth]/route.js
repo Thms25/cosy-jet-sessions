13a5fdcad6d2df99eb26b2d7ce2c9e69df6efcd4
thomas allen <thomasallenmartinho@gmail.com>
2023-12-17 14:37:57 +0100

    auth api working

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,13 @@
-import nextAuth from "next-auth";
 import NextAuth from "next-auth/next";
-import Providers from "next-auth/providers";
+import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-  Providers: [
-    Providers.Google({
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-};
+});
 
-export const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
