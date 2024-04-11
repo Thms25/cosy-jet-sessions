ef2d4a6c5b47c827dd17c7bfc86763ae9024988d
Thomas Allen <thomasallenmartinho@gmail.com>
2024-04-11 16:02:38 +0200

    removed chec for users

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,29 @@
 // Styles
-import "../styles/globals.scss";
+import '../styles/globals.scss'
 
 // Auth
-import { getServerSession } from "next-auth";
-import { AuthOptions } from "./api/auth/[...nextauth]/route";
+import { getServerSession } from 'next-auth'
+import { AuthOptions } from './api/auth/[...nextauth]/route'
 
 // Components
-import Provider from "@/components/auth/Provider";
-import Navbar from "@/components/Nav/Navbar";
-import Footer from "@/components/Footer";
-import { findUser } from "@/utils/fetchUtils/UserFetchUtils";
+import Provider from '@/components/auth/Provider'
+import Navbar from '@/components/Nav/Navbar'
+import Footer from '@/components/Footer'
+// import { findUser } from "@/utils/fetchUtils/UserFetchUtils";
 
 export const metadata = {
-  title: "Cosy Jet Sessions",
-  description: "The coziest acoutsic sessions",
+  title: 'Cosy Jet Sessions',
+  description: 'The coziest acoutsic sessions',
   icons: {
-    icon: "/images/cjsIcon.png",
-    width: "auto",
-    height: "auto",
+    icon: '/images/cjsIcon.png',
+    width: 'auto',
+    height: 'auto',
   },
-};
+}
 
 export default async function RootLayout({ children }) {
-  const session = await getServerSession(AuthOptions);
-  const user = await findUser(session?.user?.email);
+  const session = await getServerSession(AuthOptions)
+  // const user = await findUser(session?.user?.email);
 
   return (
     <html>
@@ -35,5 +35,5 @@ export default async function RootLayout({ children }) {
         </Provider>
       </body>
     </html>
-  );
+  )
 }
