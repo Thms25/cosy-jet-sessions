af184eb0e1bbe4e9080303c8d5020f02a7462209
Thomas Allen <thomasallenmartinho@gmail.com>
2024-04-11 16:00:35 +0200

    removed old files

--- utils/fetchUtils/NotionFetchUtils.js
+++ utils/fetchUtils/NotionFetchUtils.js
@@ -1,16 +1,35 @@
-const url = "http://localhost:3000/";
+import { Client } from '@notionhq/client'
+
+export const revalidate = 60 * 60 * 24 * 7 // 1 week
 
 export async function getNotionDiscover() {
   try {
-    const res = await fetch(`${url}api/notion/discover`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await res.json();
-    return data;
+    const notion = new Client({
+      auth: process.env.NOTION_SECRET,
+    })
+    const { results: blocks } = await notion.blocks.children.list({
+      block_id: process.env.NOTION_DISCOVER_ID,
+    })
+
+    const data = blocks.map(block => {
+      try {
+        if (block.type == 'paragraph') {
+          const text = block.paragraph.rich_text[0].plain_text
+          const [key, value] = text.split(': ')
+
+          return { [key]: value }
+        }
+
+        return {}
+      } catch (error) {
+        console.log('Error in a notion block in discover page')
+      }
+    })
+
+    const content = data.reduce((acc, obj) => ({ ...acc, ...obj }), {})
+
+    return content
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error)
   }
 }
